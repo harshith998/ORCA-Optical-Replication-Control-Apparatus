917419137cd619bffe9600fd48646fc4f8307e99
Michael Danley <michael.danley@duke.edu>
2025-11-04 12:50:31 -0500

    Pico ADC code finished. Baud set 115200 over TX. Read analog pin 26

--- software/pico/src/main.cpp
+++ software/pico/src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
 
+/**
+ * This code is intended to utilize the ADC capabilities of the Pi PICO
+ * It converts an analog voltage reading on a specified GPIO (3.3V Max) to UART over the default TX pin
+ */
+
+ // Configuration Constants
+const unsigned long SAMPLE_MS = 50; // 20 Hz sampling
+const unsigned int baudRate = 115200; // Baud rate
+unsigned long lastSample = 0;
+const unsigned int analogPin = 26;
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(baudRate);
   delay(500); // Give serial time to initialize
 }
 
 void loop() {
-  int raw = analogRead(26);               // GP26 (A0) = ADC0
-  float voltage = raw * (3.3f / 4095.0f); // Convert to volts, 12-bit ADC (0–4095)
-//   Serial.print("Voltage: ");
-  Serial.println(voltage, 3);
-  delay(100); // 10 Hz update
+  // Sample data every SAMPLE_MS milliseconds
+  unsigned long currentTime = millis();
+  if (currentTime - lastSample >= SAMPLE_MS) {
+    lastSample = currentTime;
+
+    // Read voltage from the analog pin and transmit over UART
+    int raw = analogRead(analogPin);
+    float voltage = raw * (3.3f / 4095.0f); // Convert to volts, 12-bit ADC (0–4095)
+    Serial.println(voltage, 3);
+  }
 }
