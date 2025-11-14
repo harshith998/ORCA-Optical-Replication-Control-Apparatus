761e16855f57139c50eb3186d9f150f33534c8a1
Michael Danley <michael.danley@duke.edu>
2025-11-14 12:41:51 -0500

    bruh

---
+++ software/ESP32/.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ /Users/michaeldanley/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /Users/michaeldanley/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/michaeldanley/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
