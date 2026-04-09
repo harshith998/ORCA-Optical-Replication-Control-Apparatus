da1a34611a23b848fc2bcc96e074bdb3a8fc64d1
Michael Danley <michael.danley@duke.edu>
2026-04-09 11:17:01 -0400

    idk

--- software/chamber-pi/tests/lora.py
+++ software/chamber-pi/tests/lora.py
@@ -4,75 +4,38 @@ sys.path.append(os.path.dirname(os.path.dirname(currentdir)))
 from LoRaRF import SX126x
 import time
 
-# Begin LoRa radio: SPI bus 0, CE1 (spidev0.1), reset=BCM8, busy=BCM20, irq/DIO1=BCM21
 LoRa = SX126x()
 print("Begin LoRa radio")
-if not LoRa.begin(0, 1, 8, 20, -1) :
+if not LoRa.begin(0, 1, 8, 20, -1):
     raise Exception("Something wrong, can't begin LoRa radio")
 
-# Set frequency to 915 Mhz
-print("Set frequency to 915 Mhz")
 LoRa.setFrequency(915000000)
+LoRa.setRxGain(LoRa.RX_GAIN_BOOSTED)
 
-# Set RX gain to power saving gain
-print("Set RX gain to power saving gain")
-LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING)
-
-# Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
-print("Set modulation parameters:\n\tSpreading factor = 9\n\tBandwidth = 250 kHz\n\tCoding rate = 4/7")
 sf = 9
 bw = 250000
 cr = 7
 LoRa.setLoRaModulation(sf, bw, cr)
-
-# Configure packet parameter including header type, preamble length, payload length, and CRC type
-print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
-headerType = LoRa.HEADER_EXPLICIT
-preambleLength = 12
-payloadLength = 15
-crcType = True
-LoRa.setLoRaPacket(headerType, preambleLength, payloadLength, crcType)
-
-# Set syncronize word for public network (0x3444)
-print("Set syncronize word to 0x1424 (private, matches satellite 0x12)")
+LoRa.setLoRaPacket(LoRa.HEADER_EXPLICIT, 12, 255, True)
 LoRa.setSyncWord(0x1424)
 
-print("\n-- Transmitting test packet --")
-LoRa.beginPacket()
-LoRa.write(0x54)  # 'T'
-LoRa.write(0x45)  # 'E'
-LoRa.write(0x53)  # 'S'
-LoRa.write(0x54)  # 'T'
-LoRa.endPacket()
-print("Test packet sent\n")
-
-print("-- LoRa Receiver Continuous --\n")
-
-# Request for receiving new LoRa packet in RX continuous mode
+print("Listening for packets (915 MHz, BW250, SF9, CR7, sync=0x1424)...\n")
 LoRa.request(LoRa.RX_CONTINUOUS)
 
-# Receive message continuously
-while True :
+while True:
+    if LoRa.available():
+        # Drain the buffer
+        length = 0
+        while LoRa.available():
+            LoRa.read()
+            length += 1
 
-    # Check for incoming LoRa packet
-    if LoRa.available() :
+        rssi = LoRa.packetRssi()
+        snr  = LoRa.snr()
+        print(f"PKT len={length:3d}  RSSI={rssi:7.2f} dBm  SNR={snr:6.2f} dB")
 
-        # Put received packet to message and counter variable
-        message = ""
-        while LoRa.available() > 1 :
-            message += chr(LoRa.read())
-        counter = LoRa.read()
-
-        # Print received message and counter in serial
-        print(f"{message}  {counter}")
-
-        # Print packet/signal status including RSSI, SNR, and signalRSSI
-        print("Packet status: RSSI = {0:0.2f} dBm | SNR = {1:0.2f} dB".format(LoRa.packetRssi(), LoRa.snr()))
-
-        # Show received status in case CRC or header error occur
         status = LoRa.status()
-        if status == LoRa.STATUS_CRC_ERR : print("CRC error")
-        if status == LoRa.STATUS_HEADER_ERR : print("Packet header error")
+        if status == LoRa.STATUS_CRC_ERR:    print("  !! CRC error")
+        if status == LoRa.STATUS_HEADER_ERR: print("  !! Header error")
 
-        # Re-enter RX continuous mode for next packet
         LoRa.request(LoRa.RX_CONTINUOUS)
