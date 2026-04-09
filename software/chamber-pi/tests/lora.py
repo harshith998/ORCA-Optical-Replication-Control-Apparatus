add25ae575ab9c31974244fba0dd17907baad427
Michael Danley <michael.danley@duke.edu>
2026-04-09 13:52:20 -0400

    more claude nonsense

--- software/chamber-pi/tests/lora.py
+++ software/chamber-pi/tests/lora.py
@@ -1,18 +1,15 @@
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(os.path.dirname(currentdir)))
-from LoRaRF import SX126x, LoRaSpi, LoRaGpio
+from LoRaRF import SX126x
 import time
 
-# Begin LoRa radio with connected SPI bus and IO pins (cs and reset) on GPIO
-# SPI is defined by bus ID and cs ID and IO pins defined by chip and offset number
-spi = LoRaSpi(0, 0)
-cs = LoRaGpio(0, 8)
-reset = LoRaGpio(0, 24)
-busy = LoRaGpio(0, 23)
-LoRa = SX126x(spi, cs, reset, busy)
+# Begin LoRa radio using legacy initialization
+# begin(bus, device, reset, busy, irq)
+# SPI Port 0, Device 1 (CE1), NRESET=BCM8, BUSY=BCM20, DIO1=BCM21
+LoRa = SX126x()
 print("Begin LoRa radio")
-if not LoRa.begin() :
+if not LoRa.begin(0, 1, 8, 20, 21) :
     raise Exception("Something wrong, can't begin LoRa radio")
 
 # Configure LoRa to use TCXO with DIO3 as control
@@ -23,29 +20,26 @@ LoRa.setDio3TcxoCtrl(LoRa.DIO3_OUTPUT_1_8, LoRa.TCXO_DELAY_10)
 print("Set frequency to 915 Mhz")
 LoRa.setFrequency(915000000)
 
-# Set RX gain. RX gain option are power saving gain or boosted gain
+# Set RX gain
 print("Set RX gain to power saving gain")
-LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING)                       # Power saving gain
+LoRa.setRxGain(LoRa.RX_GAIN_POWER_SAVING)
 
-# Configure modulation parameter including spreading factor (SF), bandwidth (BW), and coding rate (CR)
-# Receiver must have same SF and BW setting with transmitter to be able to receive LoRa packet
+# Configure modulation parameters
 print("Set modulation parameters:\n\tSpreading factor = 7\n\tBandwidth = 125 kHz\n\tCoding rate = 4/5")
-sf = 7                                                          # LoRa spreading factor: 7
-bw = 125000                                                     # Bandwidth: 125 kHz
-cr = 5                                                          # Coding rate: 4/5
+sf = 7
+bw = 125000
+cr = 5
 LoRa.setLoRaModulation(sf, bw, cr)
 
-# Configure packet parameter including header type, preamble length, payload length, and CRC type
-# The explicit packet includes header contain CR, number of byte, and CRC type
-# Receiver can receive packet with different CR and packet parameters in explicit header mode
+# Configure packet parameters
 print("Set packet parameters:\n\tExplicit header type\n\tPreamble length = 12\n\tPayload Length = 15\n\tCRC on")
-headerType = LoRa.HEADER_EXPLICIT                               # Explicit header mode
-preambleLength = 12                                             # Set preamble length to 12
-payloadLength = 15                                              # Initialize payloadLength to 15
-crcType = True                                                  # Set CRC enable
+headerType = LoRa.HEADER_EXPLICIT
+preambleLength = 12
+payloadLength = 15
+crcType = True
 LoRa.setLoRaPacket(headerType, preambleLength, payloadLength, crcType)
 
-# Set syncronize word for public network (0x3444)
+# Set sync word for public network
 print("Set syncronize word to 0x3444")
 LoRa.setSyncWord(0x3444)
 
@@ -53,27 +47,17 @@ print("\n-- LoRa Receiver --\n")
 
 # Receive message continuously
 while True :
-
-    # Request for receiving new LoRa packet
     LoRa.request()
-    # Wait for incoming LoRa packet
     LoRa.wait()
 
-    # Put received packet to message and counter variable
-    # read() and available() method must be called after request() or listen() method
     message = ""
-    # available() method return remaining received payload length and will decrement each read() or get() method called
     while LoRa.available() > 1 :
         message += chr(LoRa.read())
     counter = LoRa.read()
 
-    # Print received message and counter in serial
     print(f"{message}  {counter}")
-
-    # Print packet/signal status including RSSI, SNR, and signalRSSI
     print("Packet status: RSSI = {0:0.2f} dBm | SNR = {1:0.2f} dB".format(LoRa.packetRssi(), LoRa.snr()))
 
-    # Show received status in case CRC or header error occur
     status = LoRa.status()
     if status == LoRa.STATUS_CRC_ERR : print("CRC error")
     elif status == LoRa.STATUS_HEADER_ERR : print("Packet header error")
